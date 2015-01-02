0e8c3b54474adb0e7d0b9acd7dfec048f1396882
LJ Sigersmith <LJ@LJs-MacBook-Pro.local>
2015-01-01 20:22:15 -0500

    Initial Commit

---
+++ Fake Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fake Calculator
+//
+//  Created by LJ Sigersmith on 1/1/15.
+//  Copyright (c) 2015 LJ Sigersmith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
