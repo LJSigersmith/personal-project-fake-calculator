e6a42267c30069e3cd765353f17f13dd503f2f84
TonyStarkMark42 <lancedjunior@gmail.com>
2015-01-05 20:26:16 -0500

    Added Fake Calculator App

--- Fake Calculator/ViewController.h
+++ Fake Calculator/ViewController.h
@@ -7,8 +7,44 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface ViewController : UIViewController
+int ResultNumber;
+int DisplayResult;
+@interface ViewController : UIViewController {
+    IBOutlet UIButton *One;
+    IBOutlet UIButton *Two;
+    IBOutlet UIButton *Three;
+    IBOutlet UIButton *Four;
+    IBOutlet UIButton *Five;
+    IBOutlet UIButton *Six;
+    IBOutlet UIButton *Seven;
+    IBOutlet UIButton *Eight;
+    IBOutlet UIButton *Nine;
+    IBOutlet UIButton *Zero;
+    IBOutlet UIButton *Plus;
+    IBOutlet UIButton *Minus;
+    IBOutlet UIButton *Times;
+    IBOutlet UIButton *Divide;
+    IBOutlet UIButton *Clear;
+    IBOutlet UIButton *Equals;
+    IBOutlet UILabel *Result;
+    
+}
+-(IBAction)One:(id)sender;
+-(IBAction)Two:(id)sender;
+-(IBAction)Three:(id)sender;
+-(IBAction)Four:(id)sender;
+-(IBAction)Five:(id)sender;
+-(IBAction)Six:(id)sender;
+-(IBAction)Seven:(id)sender;
+-(IBAction)Eight:(id)sender;
+-(IBAction)Nine:(id)sender;
+-(IBAction)Zero:(id)sender;
+-(IBAction)Plus:(id)sender;
+-(IBAction)Minus:(id)sender;
+-(IBAction)Times:(id)sender;
+-(IBAction)Divide:(id)sender;
+-(IBAction)Clear:(id)sender;
+-(IBAction)Equals:(id)sender;
 
 
 @end
--- Fake Calculator/ViewController.m
+++ Fake Calculator/ViewController.m
@@ -9,11 +9,98 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
+-(IBAction)One:(id)sender {
+    Result.text = [NSString stringWithFormat: @"%d", 1];
+}
+-(IBAction)Two:(id)sender {
+    Result.text = [NSString stringWithFormat: @"%d", 2];
+}
 
+-(IBAction)Three:(id)sender {
+    Result.text = [NSString stringWithFormat: @"%d", 3];
+}
+
+-(IBAction)Four:(id)sender {
+    Result.text = [NSString stringWithFormat: @"%d", 4];
+}
+-(IBAction)Five:(id)sender {
+    Result.text = [NSString stringWithFormat: @"%d", 5];
+}
+-(IBAction)Six:(id)sender {
+    Result.text = [NSString stringWithFormat: @"%d", 6];
+}
+-(IBAction)Seven:(id)sender {
+    Result.text = [NSString stringWithFormat: @"%d", 7];
+}
+-(IBAction)Eight:(id)sender {
+    Result.text = [NSString stringWithFormat: @"%d", 8];
+}
+-(IBAction)Nine:(id)sender {
+    Result.text = [NSString stringWithFormat: @"%d", 9];
+}
+-(IBAction)Zero:(id)sender {
+    Result.text = [NSString stringWithFormat: @"%d", 0];
+}
+-(IBAction)Plus:(id)sender {
+    Result.text = [NSString stringWithFormat: @"+"];
+}
+-(IBAction)Minus:(id)sender {
+    Result.text = [NSString stringWithFormat: @"-"];
+}
+-(IBAction)Times:(id)sender {
+    Result.text = [NSString stringWithFormat: @"X"];
+}
+-(IBAction)Divide:(id)sender {
+    Result.text = [NSString stringWithFormat: @"/"];
+}
+-(IBAction)Clear:(id)sender {
+    Result.text = [NSString stringWithFormat: @""];
+}
+-(IBAction)Equals:(id)sender {
+    ResultNumber = arc4random() %11;
+    if (ResultNumber==0) {
+        Result.text = [NSString stringWithFormat: @"Yes"];
+    }
+    if (ResultNumber==1) {
+        Result.text = [NSString stringWithFormat: @"No"];
+    }
+    if (ResultNumber==2) {
+        Result.text = [NSString stringWithFormat: @"Error"];
+    }
+    if (ResultNumber==3) {
+        Result.text = [NSString stringWithFormat: @"SWAG"];
+    }
+    if (ResultNumber==4) {
+        Result.text = [NSString stringWithFormat: @"LOL TOO LAZY"];
+    }
+    if (ResultNumber==5) {
+        Result.text = [NSString stringWithFormat: @"Yawn..."];
+    }
+    if (ResultNumber==6) {
+        DisplayResult = arc4random() %2001;
+        Result.text = [NSString stringWithFormat:@"%d", DisplayResult];
+    }
+    if (ResultNumber==7) {
+        DisplayResult = arc4random() %5008;
+        Result.text = [NSString stringWithFormat:@"%d", DisplayResult];
+    }
+    if (ResultNumber==8) {
+        DisplayResult = arc4random() %7;
+        Result.text = [NSString stringWithFormat:@"%d", DisplayResult];
+    }
+    if (ResultNumber==9) {
+        DisplayResult = arc4random() %8983;
+        Result.text = [NSString stringWithFormat:@"%d", DisplayResult];
+    }
+    if (ResultNumber==10) {
+        DisplayResult = arc4random() %54;
+        Result.text = [NSString stringWithFormat:@"%d", DisplayResult];
+    }
+    
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
